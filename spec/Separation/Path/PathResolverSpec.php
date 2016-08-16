0699e263db08cf360678eb86401007fae95c8042
Andrew Kirkpatrick <a.kirkpatrick@outlook.com>
2016-08-16 11:58:15 +0100

    Checking that top-level components are hooked up correctly

--- spec/Separation/Path/PathResolverSpec.php
+++ spec/Separation/Path/PathResolverSpec.php
@@ -2,21 +2,39 @@
 
 namespace spec\Separation\Path;
 
+use PhpCollection\Sequence;
 use Separation\Path\Adapter\AdapterInterface;
+use Separation\Path\Path;
 use Separation\Path\PathFactory;
 use Separation\Path\PathResolver;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Separation\Repository;
+use Separation\User;
 
 class PathResolverSpec extends ObjectBehavior
 {
+    private $pathFactory;
+
     function let(AdapterInterface $adapter, PathFactory $pathFactory)
     {
+        $this->pathFactory = $pathFactory;
+
         $this->beConstructedWith($adapter, $pathFactory);
     }
 
     function it_is_initializable()
     {
         $this->shouldHaveType(PathResolver::class);
+    }
+
+    function it_should_resolve_the_path_of_repositories_between_two_users()
+    {
+        $user1 = new User('archer');
+        $user2 = new User('lana');
+        $repositories = new Sequence([new Repository('archer/burgers')]);
+        $this->pathFactory->create($user1, $user2, $repositories)->willReturn(new Path($user1, $user2, $repositories));
+
+        $this->resolvePathBetweenUsers($user1, $user2)->shouldBeAnInstanceOf('Separation\Path\Path');
     }
 }
--- src/Separation/Path/PathResolver.php
+++ src/Separation/Path/PathResolver.php
@@ -2,7 +2,10 @@
 
 namespace Separation\Path;
 
+use PhpCollection\Sequence;
 use Separation\Path\Adapter\AdapterInterface;
+use Separation\Repository;
+use Separation\User;
 
 class PathResolver
 {
@@ -14,5 +17,10 @@ class PathResolver
     {
         $this->adapter = $adapter;
         $this->pathFactory = $pathFactory;
+    }
+
+    public function resolvePathBetweenUsers(User $user1, User $user2)
+    {
+        return $this->pathFactory->create($user1, $user2, new Sequence([new Repository('archer/burgers')]));
     }
 }
